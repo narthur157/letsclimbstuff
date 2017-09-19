95cb347bb281e1423dcacf600d1bb5f89807ae78
Nicholas Arthur <narthur157@gmail.com>
2017-09-19 10:55:14 -0600

    Make project npm installable

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env', 'react']
+            presets: ['react']
           }
         }
       }
